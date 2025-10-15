2df1d7906f21ccc2b4a7088aa99385b818d99242
Richard Liu <richardadesigner@gmail.com>
2025-10-14 23:27:50 -0700

    update

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 let EXTENSIONSBOTTOMBAR = document.createElement("div");
-EXTENSIONSBOTTOMBAR.style = "user-select:none;width:160px;height:20px;position:fixed;right:0;bottom:0px;background-color:white;border-radius:5px 0px 0px 0px;font-family:arial;display:flex;align-items:center;justify-content:left;border-style:solid;border-width:1px 0px 0px 1px;border-color:rgba(200, 198, 198, 1);font-size:14px;"
+EXTENSIONSBOTTOMBAR.style = "all:unset;margin-bottom:0px;margin-top:0px;z-index:99999995;user-select:none;width:160px;height:20px;position:fixed;right:0;bottom:0px;background-color:white;border-radius:5px 0px 0px 0px;font-family:arial;display:flex;align-items:center;justify-content:left;border-style:solid;border-width:1px 0px 0px 1px;border-color:rgba(200, 198, 198, 1);font-size:14px;"
 document.body.appendChild(EXTENSIONSBOTTOMBAR);
 setInterval(function () {
     let EXTENSIONCLOCKHOUR = new Date().getHours();
@@ -23,5 +23,5 @@ setInterval(function () {
     if (EXTENSIONCLOCKSECOND.toString().length == 1) {
         EXTENSIONCLOCKSECOND = "0" + EXTENSIONCLOCKSECOND
     }
-    EXTENSIONSBOTTOMBAR.innerHTML = "<p style=\"margin-left:3px;padding:0px;margin-bottom:0px;\">" + "<span>" + EXTENSIONCLOCKHOUR + ":" + EXTENSIONCLOCKMINUTE + ":" + EXTENSIONCLOCKSECOND + "</span>" + "<span style=\"color:lightgray\"> | </span>" + "<span>" + (new Date().getMonth() + 1) + "/" + new Date().getDate() + "/" + new Date().getFullYear() + "</span>" + "</p>";
+    EXTENSIONSBOTTOMBAR.innerHTML = "<p style=\"all:unset;margin-left:3px;padding:none;margin-bottom:0px;margin-top:0px;\">" + "<span>" + EXTENSIONCLOCKHOUR + ":" + EXTENSIONCLOCKMINUTE + ":" + EXTENSIONCLOCKSECOND + "</span>" + "<span style=\"color:lightgray\"> | </span>" + "<span>" + (new Date().getMonth() + 1) + "/" + new Date().getDate() + "/" + new Date().getFullYear() + "</span>" + "</p>";
 }, 500)
